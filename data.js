dcd509a25119c44d62440867d59d1fa81f9a5b5a
TeleNaijaDev <68859919+TeleNaijaDev@users.noreply.github.com>
2020-07-27 15:24:50 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,5 @@
+module.exports = {
+  token: '926187237:AAGUexMDXy-_hHIX2oWjCEslkM08wVPMbZI', // token from @BotFather
+  mongoLink: 'mongodb+srv://<venom>:<iamvenom>@vtelebot1.6shfp.mongodb.net/<telebot>?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
+  dev: 983396004, // developer`s id for sending him/her errors
+}
